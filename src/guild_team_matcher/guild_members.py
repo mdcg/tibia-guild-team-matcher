6e6bef84fd754eda4fb4b26c477e942874c35f46
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-03-26 20:40:54 -0300

    feat: adding some docstring (#1)

--- src/guild_team_matcher/guild_members.py
+++ src/guild_team_matcher/guild_members.py
@@ -3,21 +3,52 @@ from bs4 import BeautifulSoup
 
 
 def get_guild_members(guild_name):
+    """Simple data scraping on the Guild page informed by the parameter.
+
+    Args:
+        guild_name (str): Guild name.
+
+    Returns:
+        bs4.element.Tag: Object with all items in the table referring to Guild
+        member data.
+    """
     GUILD_TABLE_INDEX = 6
-    raw_guild_details_html = requests.post("https://www.tibia.com/community/?subtopic=guilds&page=view", data={"GuildName": guild_name}).text
-    soup = BeautifulSoup(raw_guild_details_html, 'html.parser')
+
+    raw_guild_details_html = requests.post(
+        "https://www.tibia.com/community/?subtopic=guilds&page=view",
+        data={"GuildName": guild_name},
+    ).text
+
+    soup = BeautifulSoup(raw_guild_details_html, "html.parser")
     members_table = soup.find_all("table")[GUILD_TABLE_INDEX]
-    return members_table
+
+    return type(members_table)
 
 
 def filter_members_by_vocation(guild_members):
+    """Filters guild members by separating them into their respective
+    vocations. To facilitate future consultations, we have separated some
+    information by member: Name, Level and details (link about the char in
+    Tibia).
+
+    Args:
+        guild_members (bs4.element.Tag): Information about guild members is
+        contained in a table on the Tibia website. Basically, this parameter is
+        just this table.
+
+    Returns:
+        dict: Dictionary where keys are Tibia vocations and values ​​are list
+        of members belonging to specific vocation.
+    """
+    SKIP_HEADER_TR = 1
+
     members_peer_vocation = {
         "Sorcerer": [],
         "Druid": [],
         "Knight": [],
-        "Paladin": [], 
+        "Paladin": [],
     }
-    SKIP_HEADER_TR = 1
+
     for member_info_tr in guild_members.find_all("tr"):
         member_info_tds = member_info_tr.find_all("td")
         try:
@@ -33,11 +64,5 @@ def filter_members_by_vocation(guild_members):
         for vocation in members_peer_vocation.keys():
             if vocation in member["vocation"]:
                 members_peer_vocation[vocation].append(member)
-    
+
     return members_peer_vocation
-
-
-if __name__ == "__main__":
-    members = get_guild_members("Sucesso")
-    for ms in filter_members_by_vocation(members)["Druid"]:
-        print(f"{ms['name']} ({ms['level']}) - {ms['details']}")
