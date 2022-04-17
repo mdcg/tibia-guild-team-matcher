284f2fb058032cc3943d8145ade3ac7c0bbf7337
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-04-16 23:56:11 -0300

    feat: initial settings and logging modules

---
+++ core/guild_team_matcher/logs.py
@@ -0,0 +1,15 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        fmt="[Sucesso] %(asctime)s - %(levelname)s : %(message)s",
+        datefmt="%d/%m/%Y %I:%M:%S %p",
+    )
+)
+
+logger.addHandler(handler)
