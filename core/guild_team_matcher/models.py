13d46f121cb61d4d706f604f9bb29b7790caac83
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-04-17 23:02:06 -0300

    feat: creating __str__ to Player model (#1)

--- core/guild_team_matcher/models.py
+++ core/guild_team_matcher/models.py
@@ -1,3 +1,5 @@
+import unicodedata
+
 from pydantic import BaseModel
 
 
@@ -6,3 +8,18 @@ class Player(BaseModel):
     level: int
     vocation: str
     details: str
+
+    def __str__(self):
+        return f"{unicodedata.normalize('NFKD', self.name)} - {self.vocation_initials()} (Lvl: {self.level})"
+
+    def vocation_initials(self):
+        initials = {
+            "Elite Knight": "EK",
+            "Master Sorcerer": "MS",
+            "Elder Druid": "ED",
+            "Royal Paladin": "RP",
+        }
+
+        for k, v in initials.items():
+            if self.vocation in k:
+                return v
